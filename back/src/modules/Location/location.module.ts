a933c9185c59ebd2a7dc063c0aeec485fbf0100c
ConstanzaHerrera-code <conty1505@gmail.com>
2024-10-24 22:19:19 -0300

    location module

--- back/src/modules/Location/location.module.ts
+++ back/src/modules/Location/location.module.ts
@@ -3,15 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { LocationController } from './location.controller';
 import { LocationService } from './location.service';
 import { LocationRepository } from './location.repository';
-import { RoomRepository } from './room.repository';
-import { RoomService } from './room.service';
-import { Room } from '../Rooms/entities/rooms.entity';
-import { Reservation } from '../../reservations/entities/reservation.entity';
+import { RoomsRepository } from '../Rooms/room.repository';
+import { RoomService } from '../Rooms/rooms.service';
+
 
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([LocationRepository, RoomRepository]),
+    TypeOrmModule.forFeature([LocationRepository, RoomsRepository]),
   ],
   controllers: [LocationController],
   providers: [LocationService, RoomService],
