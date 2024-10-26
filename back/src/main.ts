d4234da1d404bbaeaadfbd511351b621eb840a3a
“Gimena” <gimenapascuale@gmail.com>
2024-10-26 12:08:07 -0300

    Forms

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cors from 'cors'; // Importa el paquete cors si lo necesitas
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Configura CORS
+  app.enableCors({
+    origin: 'http://localhost:3001', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true, 
+  });
+
   const config = new DocumentBuilder()
     .setTitle('API de Reserva')
     .setDescription('Documentación de la API para el sistema de reservas')
@@ -14,6 +23,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  
   await app.listen(3000);
 }
 bootstrap();
