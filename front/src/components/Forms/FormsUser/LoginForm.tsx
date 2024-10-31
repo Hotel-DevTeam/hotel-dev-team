4125793274ea00f3251ada8b15f4adcbbf234044
“Gimena” <gimenapascuale@gmail.com>
2024-10-31 08:18:28 -0300

    products front

--- front/src/components/Forms/FormsUser/LoginForm.tsx
+++ front/src/components/Forms/FormsUser/LoginForm.tsx
@@ -100,6 +100,7 @@ export default function LoginForm() {
 
                     <div className="flex justify-center">
                         <button
+                         disabled={Object.keys(errors).length > 0}
                             type="submit"
                             className="inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
                         >
