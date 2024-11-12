de075820a5be792a0bda841f844c2737afcd9a31
Ignacio Jordan <162840398+Ijordan94@users.noreply.github.com>
2024-11-12 18:40:07 -0300

    seed pulleado

--- back/src/config/typeorm.ts
+++ back/src/config/typeorm.ts
@@ -1,5 +1,15 @@
 import { registerAs } from '@nestjs/config';
 import { config as dotenvConfig } from 'dotenv';
+import { Caja } from 'src/modules/caja/caja/entities/caja.entity';
+import { Movimiento } from 'src/modules/caja/movimientos/entities/movimiento.entity';
+import { Location } from 'src/modules/Location/entities/location.entity';
+import { Pax } from 'src/modules/pax/entity/pax.entity';
+import { Product } from 'src/modules/products/entities/product.entity';
+import { Reservation } from 'src/modules/reservations/entities/reservation.entity';
+import { Room } from 'src/modules/Rooms/entities/rooms.entity';
+import { SalesOrder } from 'src/modules/SalesOrder/entities/salesOrder.entity';
+import { SalesOrderLine } from 'src/modules/SalesOrderLine/entities/salesOrderLine.entity';
+import { Users } from 'src/modules/Users/entities/users.entity';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 dotenvConfig({ path: '.env.development' });
@@ -14,7 +24,7 @@ const config: DataSourceOptions = {
   dropSchema: false,
   logging: true,
   synchronize: true,
-  entities: ['dist/**/*.entity{.ts,.js}'],
+  entities: [Location,Room,Users,Caja,Product,Reservation,SalesOrder,SalesOrderLine,Pax, Movimiento],
   migrations: ['dist/migrations/*{.ts,.js}'],
 };
 
--- back/src/modules/caja/caja/caja.module.ts
+++ back/src/modules/caja/caja/caja.module.ts
@@ -3,6 +3,7 @@ import { CajaService } from './caja.service';
 import { CajaController } from './caja.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Caja } from './entities/caja.entity';
+import { Movimiento } from '../movimientos/entities/movimiento.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Caja])],
